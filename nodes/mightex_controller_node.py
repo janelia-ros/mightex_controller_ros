1b499d48a353974f56a8ae88f3900ec7eecb0be9
Peter Polidoro <peterpolidoro@gmail.com>
2016-06-06 15:08:20 -0400

    Check for WriteFrequencyError rather than set arbitrary feedback value.

--- nodes/mightex_controller_node.py
+++ nodes/mightex_controller_node.py
@@ -4,6 +4,7 @@
 from __future__ import print_function,division
 
 from mightex_device import MightexDevice
+from serial_device2 import WriteFrequencyError
 
 import rospy
 
@@ -23,13 +24,14 @@ class MightexController(object):
 
         self._dev = MightexDevice()
         rospy.loginfo('mightex_controller_node initialized!')
-        self._feedback_period = 0.25
-        self._last_update_time = 0
         self._setup = False
         self._initialized = True
 
     def _setup_device(self):
         self._channel_count = self._dev.get_channel_count()
+        self._enabled = []
+        for channel in range(self._channel_count):
+            self._enabled.append(False)
         if self._channel_count == 0:
             rospy.loginfo('mightex_controller found no device channels!')
         else:
@@ -42,34 +44,37 @@ class MightexController(object):
 
     def _cmd_current_callback(self,data):
         if self._initialized:
-            time_now = rospy.get_time()
-            if (time_now - self._last_update_time) < self._feedback_period:
-                return
-            self._last_update_time = time_now
-            rospy.loginfo('mightex_controller channel: {0}, current {1}'.format(data.channel,data.current))
-            if not self._setup:
-                self._setup_device()
-            channel = data.channel
-            current = data.current
-            if (channel >= 1) and (channel <= self._channel_count):
-                if current > 0:
-                    self._dev.set_normal_current(channel,current)
-                    self._dev.set_mode_normal(channel)
-                else:
-                    self._dev.set_mode_disable(channel)
+            # rospy.loginfo('mightex_controller channel: {0}, current {1}'.format(data.channel,data.current))
+            try:
+                if not self._setup:
+                    self._setup_device()
+                channel = data.channel
+                current = data.current
+                if (channel >= 1) and (channel <= self._channel_count):
+                    if current > 0:
+                        self._dev.set_normal_current(channel,current)
+                        if not self._enabled[channel]:
+                            self._dev.set_mode_normal(channel)
+                            self._enabled[channel] = True
+                    else:
+                        self._dev.set_mode_disable(channel)
+                        self._enabled[channel] = False
+            except WriteFrequencyError:
+                pass
 
     def _cmd_off_callback(self,data):
         if self._initialized:
-            rospy.loginfo('mightex_controller cmd_off')
+            # rospy.loginfo('mightex_controller cmd_off')
             if not self._setup:
                 self._setup_device()
             channel = data.channel
             if (channel >= 1) and (channel <= self._channel_count):
                 self._dev.set_mode_disable(channel)
+                self._enabled[channel] = False
 
     def _cmd_all_off_callback(self,data):
         if self._initialized:
-            rospy.loginfo('mightex_controller cmd_all_off')
+            # rospy.loginfo('mightex_controller cmd_all_off')
             if not self._setup:
                 self._setup_device()
             self.all_off()
@@ -81,6 +86,7 @@ class MightexController(object):
             for channel in range(self._channel_count):
                 channel += 1
                 self._dev.set_mode_disable(channel)
+                self._enabled[channel] = False
 
 
 if __name__ == '__main__':
